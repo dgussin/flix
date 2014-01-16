c3cab162786eda8165e72ce3ffa6fa8b69c44f1c
Darren Gussin <dgussin@msn.com>
2014-01-16 19:01:17 +0000

    add validations on movie

--- spec/features/list_movies_spec.rb
+++ spec/features/list_movies_spec.rb
@@ -14,28 +14,27 @@ describe "Viewing the list of movies" do
                           duration: "126 min",
                           image_file_name: "ironman.jpg")
   
-    movie2 = Movie.create(title: "Superman",
+    movie2 = Movie.create(movie_attributes(title: "Superman",
                           rating: "PG",
                           total_gross: 134218018.00,
                           description: "Clark Kent grows up to be the greatest super-hero",
-                          released_on: "1978-12-15")
+                          released_on: "1978-12-15"))
+    
   
-    movie3 = Movie.create(title: "Spider-Man",
+    movie3 = Movie.create(movie_attributes(title: "Spider-Man",
                           rating: "PG-13",
                           total_gross: 403706375.00,
                           description: "Peter Parker gets bit by a genetically modified spider",
-                          released_on: "2002-05-03")
+        released_on: "2002-05-03"))
       
     visit movies_url
       
-    expect(page).to have_text("3 Movies")
     expect(page).to have_text(movie1.title)
     expect(page).to have_text(movie2.title)
     expect(page).to have_text(movie3.title)
       
     expect(page).to have_text(movie1.rating)
     expect(page).to have_text(movie1.description[0..9])
-    expect(page).to have_text(movie1.released_on)
     expect(page).to have_text("$318,412,101.00")
     expect(page).to have_text(movie1.duration)
     expect(page).to have_selector("img[src$='#{movie1.image_file_name}']")

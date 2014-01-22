02bb715c3d113976376482f3ad7d86aba38a9689
Darren Gussin <dgussin@msn.com>
2014-01-22 19:03:26 +0000

    Add Review resource

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -10,6 +10,7 @@ class Movie < ActiveRecord::Base
   }
   validates :rating, inclusion: { in: RATINGS }
   
+  has_many :reviews, dependent: :destroy
   
   def flop?
     total_gross.blank? || total_gross < 50000000.00

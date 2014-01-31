a38e10b801e491361ed15df0882e8f7f624a6d1f
Darren Gussin <dgussin@msn.com>
2014-01-31 19:15:27 +0000

    add config/initializers/secret_token.rb so I can push to heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,13 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Flix::Application.config.secret_key_base = 'b4722bd54dbdec7084f0caf1d34aec862480d549ef7da9bd036da6aefc76b74583a56af5d6e8c646e750d04a29ca8e28aa5659da313da89ed9ade2eb037c12fe'
+ 

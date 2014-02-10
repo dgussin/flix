73a68c0dd2fd1d35b349a464fca7e87530de7c63
Darren Gussin <dgussin@msn.com>
2014-02-10 22:48:26 +0000

    add config for paperclip

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,10 +80,10 @@ Flix::Application.configure do
   
   #Configure Paperclip for production
   config.paperclip_defaults = {
-  :storage => :s3,
-  :url => ':s3_domain_url',
-  :path => '/:class/:attachment/:id_partition/:style/:filename',
-  :s3_credentials => {
+    :storage => :s3,
+    :url => ':s3_domain_url',
+    :path => '/:class/:attachment/:id_partition/:style/:filename',
+    :s3_credentials => {
       :bucket => ENV['AWS_BUCKET'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
